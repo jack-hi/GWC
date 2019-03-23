5a6c0b3fb73702384a5a63631085f9d6a50c92a1
jack <hjj414@gmail.com>
2019-03-23 13:56:17 +0800

    TCP test

--- packet-dispatcher.py
+++ packet-dispatcher.py
@@ -2,74 +2,80 @@
 # -*- coding: utf-8 -*-
 
 import asyncore
-from segment import Packet, Segment, SimpleWrap
 import socket
 import threading
-import struct
 import logging
+import queue
+import time
+import segment
 
 SERVER_ADDRESS = ("", 7896)
 
-log = logging.getLogger("seg.disp")
-log.setLevel(logging.DEBUG)
+Log = logging.getLogger("App0")
+Log.setLevel(logging.DEBUG)
 
+s_handler = logging.StreamHandler()  # sys.stderr
+s_handler.setLevel(logging.DEBUG)
+s_handler.setFormatter(logging.Formatter("%(asctime)s %(name)s [%(levelname)s] %(funcName)s(): %(message)s"))
+Log.addHandler(s_handler)
 
-class PacketDispatcher:
-    """
-         TCP <------> UDP
-    """
-    def __init__(self, ip, port):
-        self.tcp_handler = TcpHandler(ip, port)
-        self.udp_handler = UdpHandler(port)
-        self._thread = threading.Thread(target=asyncore.loop)
-
-    def start(self):
-        self.tcp_handler.set_udp_handler(self.udp_handler)
-        self.udp_handler.set_tcp_handler(self.tcp_handler)
-        self._thread.start()
+f_handler = logging.FileHandler("app.log")
+f_handler.setLevel(logging.DEBUG)
+f_handler.setFormatter(logging.Formatter("%(asctime)s %(name)s [%(levelname)s] %(module)s::%(funcName)s(): %(message)s"))
+Log.addHandler(f_handler)
 
 
 class TcpHandler(asyncore.dispatcher):
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, wq, rq):
         super().__init__()
         self.create_socket()
         self.connect((ip, port))
         self.recv_buf = bytearray()
         self.send_buf = bytearray()
-        self.udp_handler = None
+        # handle recved data
+        self.wfhd = None
+        self._dec_func = None
+        # send data
+        self.wq = wq
+        self.rq = rq
 
-    def set_udp_handler(self, sock):
-        self.udp_handler = sock
+    def set_decode(self, func):
+        self._dec_func = func
 
-    def _forward_packet(self):
-        """
-        Get a segment packet, forward using udphandler
-        """
-        als = self.recv_buf
-        print(Packet(als))
-        '''
-        while len(als) > 1 and als[0:2] != SimpleWrap.IDENTITY:
-            als.pop(0)
-        if len(als) < 4:
-            return
-        length = struct.unpack('>H', als[2:4])[0]
-        if len(als) < length:
-            return
-        # TODO segment
-        segment = Segment(als[:len])
-        del als[:len]
-        print(segment)'''
-        # self.udp_handler.send_queue.append([segment.get_all(), ('', 0xbac1)])
+    def decode(self):
+        if self._dec_func is not None:
+            len, self.wfhd = self._dec_func(self.recv_buf)
+            del self.recv_buf[:len]
+        else:
+            self.wfhd = self.recv_buf[:]
+            del self.recv_buf[:]
+        return self
+
+    def handler(self):
+        if self.wfhd is not None:
+            self.rq.put(self.wfhd)
+            Log.info("Handled: " + self.wfhd.hex())
+            self.wfhd = None
 
     def handle_read(self):
-        self.recv_buf += self.recv(100)
-        self._forward_packet()
+        buf = self.recv(100)
+        if len(buf) is 0:
+            return
+        self.recv_buf += buf
+        Log.debug("Received: " + self.recv_buf.hex())
+        self.decode().handler()
 
     def handle_write(self):
-        if len(self.send_buf) == 0:
-            return
-        ret = self.send(self.send_buf)
-        self.send_buf = self.send_buf[ret:]
+        if len(self.send_buf) is 0:
+            try:
+                self.send_buf += self.wq.get_nowait()
+                self.wq.task_done()
+            except(queue.Empty):
+                return
+        if len(self.send_buf):
+            ret = self.send(self.send_buf)
+            Log.debug("Sent: " + self.send_buf[0:ret].hex())
+            del self.send_buf[:ret]
 
 
 class UdpHandler(asyncore.dispatcher):
@@ -77,24 +83,8 @@ class UdpHandler(asyncore.dispatcher):
         super().__init__()
         self.create_socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.bind(('', port))
-        self.recv_queue = {}
-        self.send_queue = []
-        self.tcp_handler = None
-
-    def set_tcp_handler(self, sock):
-            self.tcp_handler = sock
-
-    def _forward_packet(self):
-        for addr, data in self.recv_queue.items():
-            while len(data) > 0 and data[0] != Segment.INDENTITY:
-                data.pop(0)
-            if len(data) < 4:
-                continue
-            length = struct.unpack('>H', data[2:4])[0]
-            if len(data) < length:
-                continue
-            self.tcp_handler.send_buf += data[:length]
-            del data[:length]
+        self.recv_queue = set() # item: {addr: message}
+        self.send_queue = list() # item: [message, addr]
 
     def handle_read(self):
         data, addr = self.socket.recvfrom(100)
@@ -103,10 +93,6 @@ class UdpHandler(asyncore.dispatcher):
         else:
             self.recv_queue[addr] += data
 
-        self._forward_packet()
-
-        # TODO received data
-
     def handle_write(self):
         if len(self.send_queue) == 0:
             return
@@ -114,16 +100,44 @@ class UdpHandler(asyncore.dispatcher):
             self.send_queue.pop(0)
             return
         ret = self.socket.sendto(*self.send_queue[0])
-        # self.send_queue[0][0] = self.send_queue[0][0][ret:]
         del self.send_queue[0][0][0:ret]
 
 
+class App(threading.Thread):
+    def __init__(self, ip, port, udp_port = 0xbac0):
+        super().__init__()
+        self.running = False
+        self.state = 0
+
+        self.tcpwq = queue.Queue()
+        self.tcprq = queue.Queue()
+        self.tcp_handler = TcpHandler(ip, port, self.tcpwq, self.tcprq)
+        # self.udp_handler = UdpHandler(udp_port)
+        self._thread = threading.Thread(target=asyncore.loop)
+
+    def send(self, data):
+        self.tcpwq.put(data)
+
+    def run(self):
+        while self.running:
+            if self.state is 0:
+                self.send(segment.LgiFrame().encode().get_all())
+            try:
+                item = self.tcprq.get()
+                self.tcprq.task_done()
+                Log.info("Transport recv: " + item.hex())
+                self.state = 1
+            except(queue.Empty):
+                time.sleep(1)
+
+    def initialize(self):
+        Log.info("Start Transport thread.")
+        self._thread.start()
+
+        self.running = True
+        self.start()
+
+
 if __name__ == '__main__':
-    disp = PacketDispatcher('10.98.1.178', 7894)
-    log.info("Connect to server")
-    sr = SimpleWrap()
-    sr.update(1, 101, '10.98.1.178:7894', [i for i in range(0, 10)])
-    sr.encode()
-    print(sr)
-    disp.tcp_handler.send_buf += sr.get_all()
-    disp.start()
+    app = App('10.98.1.178', 41400)
+    app.initialize()
